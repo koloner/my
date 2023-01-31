c83d4e8ac51a689f3dd740d46039fe1008df5ecd
koloner <hosseinlatifi50@gmail.com>
2023-01-31 18:20:48 +0330

    Update deluser.sh

--- deluser.sh
+++ deluser.sh
@@ -11,7 +11,7 @@ EASYRSA_CRL_DAYS=3650 ./easyrsa gen-crl
 rm -f /etc/openvpn/crl.pem
 cp /etc/openvpn/easy-rsa/pki/crl.pem /etc/openvpn/crl.pem
 chmod 644 /etc/openvpn/crl.pem
-find /home/ -maxdepth 2 -name "$CLIENT.ovpn" -delete
+find /home/ovpn -maxdepth 2 -name "$CLIENT.ovpn" -delete
 rm -f "/root/$CLIENT.ovpn"
 sed -i "/^$CLIENT,.*/d" /etc/openvpn/ipp.txt
 cp /etc/openvpn/easy-rsa/pki/index.txt{,.bk}
