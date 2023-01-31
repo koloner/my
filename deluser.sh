f8c3c3fab19268f84295e3595de7f1d2cdc9f582
koloner <hosseinlatifi50@gmail.com>
2023-01-31 18:41:57 +0330

    Update deluser.sh

--- deluser.sh
+++ deluser.sh
@@ -11,8 +11,8 @@ EASYRSA_CRL_DAYS=3650 ./easyrsa gen-crl
 rm -f /etc/openvpn/crl.pem
 cp /etc/openvpn/easy-rsa/pki/crl.pem /etc/openvpn/crl.pem
 chmod 644 /etc/openvpn/crl.pem
-find /home/ovpn -maxdepth 2 -name "$CLIENT.ovpn" -delete
-rm -f "/root/$CLIENT.ovpn"
+find /home/ -maxdepth 2 -name "$CLIENT.ovpn" -delete
+rm -f "/root/ovpn/$CLIENT.ovpn"
 sed -i "/^$CLIENT,.*/d" /etc/openvpn/ipp.txt
 cp /etc/openvpn/easy-rsa/pki/index.txt{,.bk}
 echo "Certificate for client $CLIENT revoked."
