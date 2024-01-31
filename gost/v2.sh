3f82b24b7edebe8e881c8e8dd92b12b22bcfbe91
koloner <hosseinlatifi50@gmail.com>
2024-01-31 13:26:07 +0330

    Create v2.sh

---
+++ gost/v2.sh
@@ -0,0 +1,5 @@
+wget https://github.com/ginuerzh/gost/releases/download/v2.11.5/gost-linux-amd64-2.11.5.gz
+gunzip gost-linux-amd64-2.11.5.gz 
+chmod +x gost-linux-amd64-2.11.5 
+mv gost-linux-amd64-2.11.5 /usr/local/bin/gost
+chmod +x /usr/local/bin/gost
