8a1fdd00c480da7584426db24a8f60186bf221a0
koloner <hosseinlatifi50@gmail.com>
2024-02-02 18:36:27 +0330

    Update last.sh

--- golang/last.sh
+++ golang/last.sh
@@ -1,8 +1,9 @@
 
 wget https://go.dev/dl/go1.21.6.linux-amd64.tar.gz
 
- rm -rf /usr/local/go && tar -C /usr/local -xzf go1.21.6.linux-amd64.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.21.6.linux-amd64.tar.gz
 
-export PATH=$PATH:/usr/local/go/bin
+#export PATH=$PATH:/usr/local/go/bin
+echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bash_profile
 
- go version
+go version
